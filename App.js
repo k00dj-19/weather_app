17ac44d4f538151ba3f83c1f9e21d5cd7dcbc57a
root <rlaehdwls310@khu.ac.kr>
2021-09-14 10:13:37 +0000

    About Flex

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+			<View style={styles.yellowView}></View>
+			<View style={styles.blueView}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+	yellowView: {
+		flex: 1,
+		backgroundColor: "yellow"
+	},
+	blueView: {
+		flex: 1,
+		backgroundColor: "blue"
+	}
+});
